6ea00782400d99c556fc42bdb1b0ca2e953f7feb
abdelkrimdjerrah <abdelkrimdjerrah@gmail.com>
2023-05-18 12:08:16 +0100

    implement redux toolkit provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import Router from "./dashboard/components/Router";
 import { BrowserRouter } from "react-router-dom";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <div>
+    <Provider store={store}>
       <BrowserRouter>
         <Router />
       </BrowserRouter>
-    </div>
+    </Provider>
   );
 }
 
