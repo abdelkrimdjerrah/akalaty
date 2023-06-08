ba2f5a1b651f03e782d9141858487490d8eab0b9
abdelkrimdjerrah <abdelkrimdjerrah@gmail.com>
2023-06-08 14:31:43 +0100

    upload images

--- src/dashboard/components/post/AddPost.tsx
+++ src/dashboard/components/post/AddPost.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { House } from "phosphor-react";
 import Input from "../../shared/Input";
 import ButtonSecondary from "../../shared/ButtonSecondary";
-import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import { axiosPrivate } from "../../api/axios";
 import { selectUserData } from "../../redux/userSlice";
 import { useSelector } from "react-redux";
 
@@ -15,16 +15,34 @@ function AddPost() {
     "https://plus.unsplash.com/premium_photo-1674386067478-9b05280a87d0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=850&q=80",
   ];
   const [loading, setLoading] = useState(false);
-  const axiosPrivate = useAxiosPrivate();
+  const [selectedFiles, setSelectedFiles] = useState<FileList | null>(null);
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedFiles(e.target.files);
+  };
+  const imagesFormData = new FormData();
+
   const handleAddPost = async () => {
     try {
       setLoading(true);
-      const postDetails = {
-        user,
-        text,
-        images,
-      };
-      const { data } = await axiosPrivate.post(`/api/posts`, postDetails);
+
+      if (selectedFiles) {
+        for (let i = 0; i < selectedFiles.length; i++) {
+          imagesFormData.append("images", selectedFiles[i]);
+        }
+        console.log(imagesFormData.getAll("images"));
+      }
+
+      const { data } = await axiosPrivate.post(`/api/posts`, imagesFormData, {
+        //overriding the base header in axio.ts by the new header that accept files
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        params: {
+          user: user,
+          text: text,
+        },
+      });
 
       if (!data?.success) {
         console.log("error");
@@ -44,7 +62,7 @@ function AddPost() {
           <House size={21} />
           <p className="text-sm font-medium">Add post</p>
         </div>
-
+        <input type="file" multiple onChange={handleFileChange} />
         <Input
           text="Write a text ..."
           type="text"
