ffbd86766091859d23e94925151834a6c2d7eec2
abdelkrimdjerrah <abdelkrimdjerrah@gmail.com>
2023-06-30 11:45:44 +0100

    string to objectId

--- src/dashboard/types/user/index.d.ts
+++ src/dashboard/types/user/index.d.ts
@@ -2,22 +2,16 @@ declare namespace Entities {
   export type Userrole = "Admin" | "Moderator" | "User";
 
   export interface UserEntity extends Creatable {
-    _id: string;
-
+    _id: mongoose.Schema.Types.ObjectId;
     username: string;
-
     email: string;
-
     password: string;
-
     role: Userrole;
-
     active: boolean;
-
     firstName?: string;
-
     lastName?: string;
-
     picture?: string;
+    createdAt?: Date;
+    updatedAt?: Date;
   }
 }
