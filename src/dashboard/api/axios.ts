8ec3faa4eccb607915fc4f24b6149fc1e74c17dc
abdelkrimdjerrah <abdelkrimdjerrah@gmail.com>
2023-07-05 23:17:31 +0100

    https

--- src/dashboard/api/axios.ts
+++ src/dashboard/api/axios.ts
@@ -4,7 +4,7 @@ import { selectToken } from "../redux/userSlice";
 import useRefreshToken from "../hooks/useRefreshToken";
 
 // const BASE_URL = "http://localhost:27015";
-const BASE_URL = "akalaty.vercel.app";
+const BASE_URL = "https://akalaty.vercel.app";
 
 export default axios.create({
   baseURL: BASE_URL,
