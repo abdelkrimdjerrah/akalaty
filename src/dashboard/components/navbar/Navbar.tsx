c4daaa2314d234827ba85244dbae500a6571e279
abdelkrimdjerrah <abdelkrimdjerrah@gmail.com>
2023-08-04 14:00:33 +0100

    responsive notifs

--- src/dashboard/components/navbar/Navbar.tsx
+++ src/dashboard/components/navbar/Navbar.tsx
@@ -1,42 +1,79 @@
 import Logo from "../../../assets/logo.svg";
-import { SignOut } from "phosphor-react";
+import { SignOut, X } from "phosphor-react";
 import Searchbar from "./Searchbar";
 import { useNavigate } from "react-router-dom";
 import { logoutUser } from "../../redux/userSlice";
 import { useDispatch } from "react-redux";
+import { BellRinging } from "phosphor-react";
 import { useEffect, useState } from "react";
-import axios  from "../../api/axios";
+import Notifications from "../notification/Notifications";
+import axios from "../../api/axios";
+import Modal from "../../shared/Modal";
 
 function Navbar() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [logout, setLogout] = useState(false);
-  
-    const handleLogout = async () => {
-      try {
-        dispatch(logoutUser());
-        navigate("/");
-        const { data } = await axios.post(`/api/auth/logout`);   
-      } catch (err) {}      
-    };
- 
+  const [showModalNotif, setShowModalNotif] = useState(false);
 
+  const handleLogout = async () => {
+    try {
+      dispatch(logoutUser());
+      navigate("/");
+      const { data } = await axios.post(`/api/auth/logout`);
+    } catch (err) {}
+  };
 
   return (
     <div className="bg-white w-full px-16 pt-2 pb-2 flex items-center justify-between">
-      <div onClick={() => navigate('/')} className="cursor-pointer">
+      <div onClick={() => navigate("/")} className="cursor-pointer">
         <img src={Logo} alt="" className="h-[40px]" />
       </div>
       <div className="hidden md:inline w-[35%]">
         <Searchbar />
       </div>
       <div
-        className="cursor-pointer flex gap-1 items-center"
-        onClick={handleLogout}
+        className="flex gap-3 items-center"
+        
       >
-        <p className="text-sm">Logout</p>
-        <SignOut size={23} />
+        <div className="inline lg:hidden " onClick={() => setShowModalNotif(true)}>
+          <BellRinging size={21} />
+        </div>
+
+        <div className="cursor-pointer flex gap-1 items-center" onClick={handleLogout}>
+          <p className="text-sm">Logout</p>
+          <SignOut size={23} />
+        </div>
       </div>
+
+      <div className="lg:hidden inline">
+      {showModalNotif && (
+        <Modal
+          closeModal={() => {
+            setShowModalNotif(false);
+          }}
+        >
+          <div
+            onClick={(e) => e.stopPropagation()}
+            className="max-w-[500px]  max-h-[80%] overflow-y-scroll bg-white w-[95%] md:min-w-[500px]   p-5 rounded-2xl relative"
+          >
+            <div onClick={() => setShowModalNotif(false)}>
+              <X
+                size={21}
+                className="text-gray-400 cursor-pointer hover:text-black absolute right-5 top-5"
+              />
+            </div>
+            
+            <div className="mt-[-17px]">
+              <Notifications widthfull/>
+            </div>
+
+          </div>
+        </Modal>
+      )}
+      </div>
+     
+
     </div>
   );
 }
--- src/dashboard/components/notification/Notifications.tsx
+++ src/dashboard/components/notification/Notifications.tsx
@@ -6,15 +6,21 @@ import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import { useNavigate } from "react-router-dom";
 import useGetNotificationPage from "../../hooks/useGetNotificationPage";
 
-function Notifications() {
+
+interface INotifProps{
+  widthfull?: boolean
+}
+
+function Notifications({widthfull}: INotifProps) {
 
   const { isLoading, isError, error, notificationPage } = useGetNotificationPage(1,3)
   
   const navigate = useNavigate()
 
-
+  console.log(widthfull)
+  const classname = widthfull ? ' w-full bg-white h-fit p-5 rounded-2xl ' : ' w-[280px] bg-white h-fit p-5 rounded-2xl '
   return (
-    <div className="bg-white w-[280px] h-fit p-5 rounded-2xl">
+    <div className={classname}>
       <div className="flex flex-col gap-4 items-center">
         <div className="flex gap-1 w-full">
           <BellRinging size={21} />
