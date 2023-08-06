1bb9fe4e5a7c29c80f7b2037c89e631b4af04b63
abdelkrimdjerrah <abdelkrimdjerrah@gmail.com>
2023-08-07 00:36:19 +0100

    fixed menu for mobile devices

--- src/dashboard/components/navbar/Navbar.tsx
+++ src/dashboard/components/navbar/Navbar.tsx
@@ -27,7 +27,7 @@ function Navbar() {
   };
 
   return (
-    <div className="bg-white w-full px-9 pt-2 pb-2 flex items-center justify-between">
+    <div className="bg-white w-full px-9 pt-2 pb-2 flex items-center justify-between relative">
       <div onClick={() => navigate("/")} className="cursor-pointer">
         <img src={Logo} alt="" className="h-[40px]" />
       </div>
@@ -86,42 +86,44 @@ function Navbar() {
       </div>
 
       {/* for menu modal */}
-      <div className=" lg:hidden absolute top-0 h-screen left-0">
         {showModalMenu && (
-          <Modal
-            closeModal={() => {
-              setShowModalMenu(false);
-            }}
-          >
-            <div className="h-screen w-[75%] md:w-1/2 absolute top-0 left-0 bg-white  p-5">
-              <div onClick={() => setShowModalMenu(false)}>
-                <X
-                  size={21}
-                  className="text-gray-400 cursor-pointer hover:text-black absolute right-5 top-[17px]"
-                />
-              </div>
+          <>
+          
+            <div onClick={() => {
+                setShowModalMenu(false);
+              }}
+              className="  bg-gray-900 bg-opacity-10 backdrop-blur-[1px] absolute top-0 left-0 h-screen w-full"
+            />
 
-              <div className="flex flex-col gap-2">
-                
-                <Sidebar />
-
-                <div className="fixed bottom-8 pr-11 w-[75%] md:w-1/2 ">
-                  <hr className="w-full"/>
-                  <div
-                    className="cursor-pointer flex gap-1 pl-3 mt-5"
-                    onClick={handleLogout}
-                  >
-                    <SignOut size={23} />
-                    <p className="text-sm font-medium">Logout</p>
-                  </div>
+            <div className="lg:hidden absolute z-20 top-0 bg-white h-screen w-[75%] md:w-1/2 left-0 p-5">
+                <div onClick={() => setShowModalMenu(false)}>
+                  <X
+                    size={21}
+                    className="text-gray-400 cursor-pointer hover:text-black absolute right-5 top-[17px]"
+                  />
                 </div>
 
+                <div className="flex flex-col gap-2">
+                  
+                  <Sidebar />
+
+                  <div className="fixed bottom-8 pr-11 w-[75%] md:w-1/2 ">
+                    <hr className="w-full"/>
+                    <div
+                      className="cursor-pointer flex gap-1 pl-3 mt-5"
+                      onClick={handleLogout}
+                    >
+                      <SignOut size={23} />
+                      <p className="text-sm font-medium">Logout</p>
+                    </div>
+                  </div>
+
+                </div>
               </div>
-            </div>
-          </Modal>
+          
+          </>
         )}
       </div>
-    </div>
   );
 }
 
