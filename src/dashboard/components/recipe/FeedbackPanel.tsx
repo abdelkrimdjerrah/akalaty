caa766406d398a9442ca96898b10755cfc004e19
abdelkrimdjerrah <abdelkrimdjerrah@gmail.com>
2023-08-20 13:12:17 +0100

    implementing create feedback modal

--- src/dashboard/components/recipe/FeedbackPanel.tsx
+++ src/dashboard/components/recipe/FeedbackPanel.tsx
@@ -1,23 +1,30 @@
-import { Cookie, SlidersHorizontal } from "phosphor-react";
+import { PlusCircle, SlidersHorizontal } from "phosphor-react";
 import ButtonSecondary from "../shared/ButtonSecondary";
 import { useNavigate } from "react-router-dom";
 import Select from "../shared/Select";
+import { useState } from "react";
+import CreateFeedbackModal from "./CreateFeedbackModal";
 
-function FeedbackPanel({filterStates}: any) {
+interface FeedbackPanelProps {
+  filterStates: {
+    optionHasImages: { value: string; label: string };
+    setOptionHasImages: React.Dispatch<React.SetStateAction<{ value: string; label: string }>>;
+    optionOrderByRating: { value: string; label: string };
+    setOptionOrderByRating: React.Dispatch<React.SetStateAction<{ value: string; label: string }>>;
+  };
+  recipeId: string;
+}
+function FeedbackPanel({filterStates, recipeId}: FeedbackPanelProps) {
 
   const {optionHasImages, setOptionHasImages, optionOrderByRating, setOptionOrderByRating} = filterStates
+  const [showModal, setShowModal] = useState(false);
 
   const navigate = useNavigate();
 
   const optionsSelectBy = [
-    { value: 'breakfast', label: 'Breakfast' },
-    { value: 'appetizer', label: 'Appetizer' },
-    { value: 'main', label: 'Main' },
-    { value: 'dessert', label: 'Dessert' },
-    { value: 'drink', label: 'Drink' },
-    { value: 'vegan', label: 'Vegan' },
-    { value: 'other', label: 'Other' },
-    { value: '', label: 'None' }
+    { value: 'with_images', label: 'With images' },
+    { value: 'without_images', label: 'Without images' },
+    { value: '', label: 'All' }
   ]
 
   const optionsOrderBy = [
@@ -29,16 +36,16 @@ function FeedbackPanel({filterStates}: any) {
   ]
 
   return (
-    <div className="w-full bg-white rounded-2xl relative px-3 py-4 sm:p-5 sm:py-6 flex flex-col xs:flex-row gap-2 xs:gap-6">
+    <div className="w-full bg-white rounded-2xl relative flex flex-col xs:flex-row gap-2 xs:gap-6">
       <div className="flex flex-col gap-3 min-w-fit">
         <div className="flex gap-1 w-full">
-          <Cookie size={21} />
-          <p className="text-sm font-medium">Add recipe</p>
+          <PlusCircle size={21} />
+          <p className="text-sm font-medium">Add feedback</p>
         </div>
 
         <div className="flex gap-2 items-center">
           <ButtonSecondary
-            onClick={() => navigate("/recipes/create")}
+            onClick={() => setShowModal(true)}
             color="gray"
           >
             Create now
@@ -75,6 +82,10 @@ function FeedbackPanel({filterStates}: any) {
 
         </div>
       </div>
+
+      {showModal && (
+        <CreateFeedbackModal closeModal={() => setShowModal(false)} recipeId={recipeId} />
+      )}
 
     </div>
   );
