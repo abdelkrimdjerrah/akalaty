49c22d4877da64783e4311396aa375f29782f299
abdelkrimdjerrah <abdelkrimdjerrah@gmail.com>
2023-08-21 13:10:39 +0100

    removing animation

--- src/dashboard/components/notification/Notifications.tsx
+++ src/dashboard/components/notification/Notifications.tsx
@@ -1,8 +1,5 @@
 import { BellRinging } from "phosphor-react";
 import NotifItem from "./NotifItem";
-import { motion, AnimatePresence } from "framer-motion";
-import { useEffect, useState } from "react";
-import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import { useNavigate } from "react-router-dom";
 import useGetNotificationPage from "../../hooks/useGetNotificationPage";
 
@@ -25,22 +22,18 @@ function Notifications() {
         <div className="flex justify-between items-center w-full">
           <div className="flex flex-col w-full">
             <ul className="flex flex-col justify-end items-center gap-2">
-              <AnimatePresence initial={false}>
                 {notificationPage &&
                   notificationPage
                     ?.slice(Math.max(notificationPage.length - 3, 0))
                     .map((notif: Entities.NotifEntity) => (
-                      <motion.li
+                      <div
                         key={notif._id}
-                        initial={{ opacity: 0, y: 50, scale: 0.2 }}
-                        animate={{ opacity: 1, y: 0, scale: 1 }}
                         className="w-full"
                       >
                         {/* Notification element */}
                         <NotifItem notif={notif} />
-                      </motion.li>
+                      </div>
                     ))}
-              </AnimatePresence>
             </ul>
           </div>
 
