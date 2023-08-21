88a29a87baa32200ad6c50140d9029eface59679
abdelkrimdjerrah <abdelkrimdjerrah@gmail.com>
2023-08-21 13:11:21 +0100

    animating modal

--- src/dashboard/components/shared/Modal.tsx
+++ src/dashboard/components/shared/Modal.tsx
@@ -1,4 +1,5 @@
-import { useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { useEffect, useState } from "react";
 
 interface Props {
   closeModal: (e: any) => void;
@@ -6,20 +7,37 @@ interface Props {
 }
 
 const Modal: React.FC<Props> = ({ closeModal, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   useEffect(() => {
     document.body.style.overflow = "hidden";
+    setIsOpen(true);
+
     return () => {
       document.body.style.overflow = "unset";
     };
   }, []);
 
   return (
-    <div
-      onClick={closeModal}
-      className="z-50  bg-gray-900 bg-opacity-10 backdrop-blur-[1px] fixed h-screen left-0 right-0 bottom-0 flex items-center justify-center"
-    >
-      {children}
-    </div>
+    <AnimatePresence  onExitComplete={() => closeModal}>
+      {isOpen && (
+        <motion.div
+          className="z-50 bg-gray-900 bg-opacity-10 backdrop-blur-[1px] fixed h-screen left-0 right-0 bottom-0 flex items-center justify-center"
+          onClick={()=> closeModal(true)}
+          initial={{ opacity: 0.2 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.3 }}
+        >
+          <motion.div
+            initial={{ y: 30 }}
+            animate={{ y: 0 }}
+            
+          >
+            {children}
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
